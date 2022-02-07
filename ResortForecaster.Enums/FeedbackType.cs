d0b2eb3f2b53c0e09cbacd2377d4ca4606d97ecf
James McGuire <mcguirepjames@gmail.com>
2022-02-07 09:48:29 -0700

    Delete unused projects, Move projects that were out of src into src

---
+++ ResortForecaster.Enums/FeedbackType.cs
@@ -0,0 +1,10 @@
+﻿namespace ResortForecaster.Enums
+{
+    public enum FeedbackType
+    {
+
+        Bug = 1,
+        Feature = 2,
+        General = 3,
+    }
+}
---
+++ ResortForecaster.Utilities/Utils.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json;
+
+namespace ResortForecaster.Utilities
+{
+    public static class Utilities
+    {
+        public static T? TryParseOut<T>(string? value) where T : struct
+        {
+            if (value == null)
+            {
+                return null;
+            }
+
+            try
+            {
+                return ReferenceEquals(value, null) ? default(T) : (T)Convert.ChangeType(value, typeof(T));
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public static string? TryGetProperty(string propertyName, JsonElement jsonElement)
+        {
+            if (jsonElement.TryGetProperty(propertyName, out var property))
+            {
+                return jsonElement.GetProperty(propertyName).ToString();
+            }
+
+            return null;
+        }
+    }
+}
